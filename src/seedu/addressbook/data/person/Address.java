9f329b51dce1788b88070a9298c21e471d8eec67
TanYongSheng <eddie.tanys@gmail.com>
2016-09-02 01:09:02 +0800

    Split Address class into Block, Street, Unit and Postal Code classes.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.data.person;
 
+import javax.swing.plaf.SliderUI;
+
 import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
@@ -7,13 +9,18 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
 public class Address {
-
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+	public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be in the following format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
+    public static final String ADDRESS_VALIDATION_REGEX = "[\\d]+([A-Z])?, .+, #[\\d]{1,2}-[\\d]{2,3}, [\\d]{6}";
 
     public final String value;
     private boolean isPrivate;
+    
+    private String[] splittedAddress;
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
 
     /**
      * Validates given address.
@@ -26,6 +33,22 @@ public class Address {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
         this.value = address;
+        
+        //Split user-entered address
+        address.replace("/a","");
+        splittedAddress = address.split(", ");
+        
+        //Create new Block object
+        this.block = new Block(splittedAddress[0]);
+        
+        //Create new Street object 
+        this.street = new Street(splittedAddress[1]);
+        
+        //Create new Unit object
+        this.unit = new Unit(splittedAddress[2]);
+        
+        //Create new PostalCode object
+        this.postalCode = new PostalCode(splittedAddress[3]);
     }
 
     /**
@@ -37,14 +60,17 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.getBlock() + ", " + street.getStreet() + ", " + unit.getUnit() + ", " + postalCode.getPostalCode() ;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+        		&& this.block.equals(((Address) other).block))
+                && this.street.equals(((Address) other).street)
+				&& this.unit.equals(((Address) other).unit)
+				&& this.postalCode.equals(((Address) other).postalCode);// state check
     }
 
     @Override
@@ -54,5 +80,97 @@ public class Address {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+}
+
+class Block{
+	private String block;
+	
+	public Block(String block){
+		this.block = block;
+	}
+	
+	public String getBlock(){
+		return block;
+	}
+	
+	public void setBlock(String newBlock){
+		this.block = newBlock;
+	}
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Block // instanceof handles nulls
+        		&& this.block == (((Block) other).block));// state check
+    }
+}
+
+class Street {
+	private String street;
+	
+	public Street(String street){
+		this.street = street;
+	}
+	
+	public String getStreet(){
+		return street;
+	}
+	
+	public void setStreet(String newStreet){
+		this.street = newStreet;
+	}
+	
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Street // instanceof handles nulls
+                && this.street == (((Street) other).street));// state check
+    }
+}
+
+class Unit {
+	private String unit;
+	
+	public Unit(String unit) {
+		this.unit = unit;
+	}
+	
+	public String getUnit(){
+		return unit;
+	}
+	
+	public void setUnit(String newUnit){
+		unit = newUnit;
+	}
+    
+	@Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Unit // instanceof handles nulls
+				&& this.unit == (((Unit) other).unit));// state check
+    }
+}
+
+class PostalCode {
+	private String postalCode;
+	
+	public PostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+	
+	public String getPostalCode(){
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+	
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+				&& this.postalCode == (((PostalCode) other).postalCode));// state check
     }
 }
